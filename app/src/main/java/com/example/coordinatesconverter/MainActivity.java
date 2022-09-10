75cb156c6a0f47e8977e32fc80fad7001cca2b7c
Denisa Suciu <denisasuciu@192-168-0-123.rdsnet.ro>
2022-09-10 14:04:13 +0300

    first page

---
+++ app/src/main/java/com/example/coordinatesconverter/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.coordinatesconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    Button start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.button2);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, ConverterActivity.class);
+        startActivity(intent);
+    }
+}
